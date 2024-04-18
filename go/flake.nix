7e5f77ddbd4a44c7aa8cf4e0b103ad2775ed4bf3
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-18 23:02:41 +0100

    style: run stylua on go->flake.nix

--- go/flake.nix
+++ go/flake.nix
@@ -6,22 +6,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        buildDeps = with pkgs; [ git go gnumake ];
-        devDeps = with pkgs; buildDeps ++ [
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      buildDeps = with pkgs; [git go gnumake];
+      devDeps = with pkgs;
+        buildDeps
+        ++ [
           golangci-lint
           gotestsum
         ];
-      in
-      {
-        devShell = pkgs.mkShell
-          {
-            buildInputs = devDeps;
-          };
-      });
+    in {
+      devShell =
+        pkgs.mkShell
+        {
+          buildInputs = devDeps;
+        };
+    });
 }
