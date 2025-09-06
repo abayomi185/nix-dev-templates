f68bcb2691cf379b9214379dbd5047bfd5c832e8
Yomi Ikuru <captyomjnr@gmail.com>
2025-09-06 21:22:41 +0100

    feat(rust_esp32_riscv): add dev flake for riscv esp32-c3 development

---
+++ rust_esp32_riscv/flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "ESP32 Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs = {
+      nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    overlays = [
+      (import rust-overlay)
+      (final: prev: {
+        rustToolchain = let
+          rust = prev.rust-bin;
+        in
+          if builtins.pathExists ./rust-toolchain.toml
+          then
+            (
+              builtins.trace "rust-toolchain.toml found"
+              rust.fromRustupToolchainFile
+              ./rust-toolchain.toml
+            )
+          else
+            (
+              builtins.trace "rust-toolchain.toml not found"
+              rust.stable.latest.default.override {
+                extensions = [
+                  "rust-src"
+                  "rustfmt"
+                ];
+              }
+            );
+      })
+    ];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo-espflash
+          esp-generate
+          ldproxy
+          probe-rs-tools
+          rustToolchain
+          taplo
+          vscode-extensions.vadimcn.vscode-lldb
+          vscode-langservers-extracted
+        ];
+
+        # for debugging with lsp in neovim
+        CODE_LLDB_PATH = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+        LIB_LLDB_PATH = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/lldb/lib/liblldb";
+      };
+    });
+  };
+}
